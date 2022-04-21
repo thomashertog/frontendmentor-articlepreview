d13d5103d0c4cf963f17cd104b502fd8ae846e56
THTAK22 <thomas.hertog@inetum-realdolmen.world>
2022-04-21 11:21:01 +0200

    make button actually work

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleShareLinks(e){
+    let shareContainer = document.querySelector('.share-container');
+    shareContainer.classList.toggle('share-links-hidden');
+    
+    let currentExpandedValue = shareButton.getAttribute('aria-expanded');
+    shareButton.setAttribute('aria-expanded', !currentExpandedValue);
+}
+
+let shareButton = document.getElementById('js-share-button');
+shareButton.addEventListener('click', toggleShareLinks);
