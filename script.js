1ee165f9f2a6c615fa40b716b77518d812389ec9
Thomas Hertog <thomas.hertog@external.toyota-europe.com>
2024-07-02 17:08:33 +0200

    fixed small mistake with toggling [aria-expanded]

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function toggleShareLinks(e){
     let shareContainer = document.querySelector('.share-container');
     shareContainer.classList.toggle('share-links-hidden');
     
-    let currentExpandedValue = shareButton.getAttribute('aria-expanded');
+    let currentExpandedValue = shareButton.getAttribute('aria-expanded') === "true";
     shareButton.setAttribute('aria-expanded', !currentExpandedValue);
 }
 
